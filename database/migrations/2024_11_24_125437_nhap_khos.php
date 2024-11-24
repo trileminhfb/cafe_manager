fb8d5112ae138fd2c35fb6fa3331c7ddd7b5cf6b
trileminhfb <leminhtrisuper@gmail.com>
2024-11-24 19:57:47 +0700

    feat: create Database nhap_khos

---
+++ database/migrations/2024_11_24_125437_nhap_khos.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Enums\nhap_khos\status_nhap_khos;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('nhap_khos', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_nha_cung_cap');
+            $table->integer('id_nhan_vien');
+            $table->string('ma_nhap_kho');
+            $table->integer('tong_tien');
+            $table->integer('tinh_trang')->default(status_nhap_khos::WAITING);
+            $table->string('ghi_chu');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('nhap_khos');
+    }
+};
