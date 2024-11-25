411b4eada7e4033b887fb38b360a72f470ba5a4b
trileminhfb <leminhtrisuper@gmail.com>
2024-11-25 10:32:49 +0700

    fix: database tong gio hang

--- database/migrations/2024_11_25_013734_tong_gio_hangs.php
+++ database/migrations/2024_11_25_013734_tong_gio_hangs.php
@@ -9,19 +9,19 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('nha_cung_caps', function (Blueprint $table) {
+        Schema::create('tong_gio_hangs', function (Blueprint $table) {
             $table->id();
             $table->integer('id_gio_hang');
             $table->integer('id_khach_hang');
-            $table->date('thanh_tien');
+            $table->integer('thanh_tien');
             $table->string('ghi_chu')->nullable();
-            $table->string('ghi_chu');
-            $table->integer('tinh_trang')->default(status_tong_gio_hangs::DONE);
+            $table->date('thoi_gian_dat');
+            $table->integer('tinh_trang')->default(status_tong_gio_hangs::WAITING);
         });
     }
 
     public function down(): void
     {
-        Schema::dropIfExists('nha_cung_caps');
+        Schema::dropIfExists('tong_gio_hangs');
     }
 };
